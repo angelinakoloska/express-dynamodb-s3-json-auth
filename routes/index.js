015133dbda1eacbd462f8820b4c23ae71b115ed8
angelk <74609669+angelinakoloska@users.noreply.github.com>
2024-04-25 09:41:30 +0200

    Update file

--- routes/index.js
+++ routes/index.js
@@ -1,49 +1,48 @@
 var express = require('express');
 var router = express.Router();
 
-const CyclicDB = require('@cyclic.sh/dynamodb');
-const db = CyclicDB(process.env.Cyclic_DB);
-let contentCollection = db.collection('content');
+const CyclicDB = require('@cyclic.sh/dynamodb')
+const db = CyclicDB(process.env.CYCLIC_DB)
+let contentCollection = db.collection('content')
 
-const AWS = require('aws-sdk');
+const AWS = require("aws-sdk");
 const s3 = new AWS.S3();
 
 // GET content
-router.get('/json', async function(req, res, next) {
-  let content = await contentCollection.get('content');
+router.get("/json", async (req, res, next) => {
+  let content = await contentCollection.get("content")
+
   if (content == null) {
     res.json({
       status: "fail",
-      message: "Content is empty, add some conntent"
+      message: "Content is empty, add some content first"
     });
   } else {
-    let contentValue = content.props.value;
+    let contentValue = content.props.value
     res.json({
       status: "success",
-      result: contentValue
+      result: contentValue,
     });
   }
 });
 
-router.post('/json', async (req, res) => {
-  let my_file = await s3.getObject({
-    Bucket: process.env.CYCLIC_BUCKET_NAME,
-    Key: "dishes.json"
-  }).promise()
-  const result = JSON.parse(my_file.Body);
-  if (result == null) {
-    res.json({
-      status:  "fail",
-      message: "Dishes is empty"
-    });
-  } else {
-    res.json({
-      status: "success",
-      result: result
-    });
+// POST content or replace content
+router.post("/json", async (req, res, next) => {
+  const { content } = req.body;
+  if (content == null) {
+    res.status(400).send("Content not provided");
+    return;
   }
+  await contentCollection.set("content", {
+    value: content
+  })
+  res.json({
+    status: "success",
+    content: content,
+  });
 });
 
+// GET all dishes
 router.get("/dishes", async (req, res, next) => {
   let my_file = await s3.getObject({
     Bucket: process.env.CYCLIC_BUCKET_NAME,
@@ -63,6 +62,7 @@ router.get("/dishes", async (req, res, next) => {
 }
 });
 
+// POST and add dishes, if exist already updates the country
 router.post("/dishes", async (req, res, next) => {
   try {
     let my_file = await s3.getObject({
@@ -112,6 +112,7 @@ router.post("/dishes", async (req, res, next) => {
   }
 });
 
+// GET the exact dish
 router.get("/dishes/:dishKey", async (req, res, next) => {
   try {
     const dish = req.params.dishKey;
@@ -150,5 +151,6 @@ router.get("/dishes/:dishKey", async (req, res, next) => {
     });
   }
 });
+
 
 module.exports = router;
